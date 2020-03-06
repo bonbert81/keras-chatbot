102ad4b14752e861d44467af2085996603f33234
Felix Alejandro Guzman <felixalejandro@outlook.com>
2020-03-05 22:49:32 -0400

    Agregando data en formato yaml

--- convertir.py
+++ convertir.py
@@ -2,25 +2,18 @@ import yaml
 import os
 import csv
 
-dir_path = "data"
+dir_path = 'data'
 files_list = os.listdir(dir_path + os.sep)
 
-
-def grouplen(sequence, chunk_size):
-    return  [sequence[i * chunk_size:(i + 1) * chunk_size] for i in range((len(sequence) + chunk_size - 1) // chunk_size )]  
-
-
-
 for filepath in files_list:
-    print("archivo: {}".format(dir_path + os.sep + filepath))
-    if filepath.endswith(".csv"):
-        stream = open(dir_path + os.sep + filepath, "r", encoding="utf-8")
+    print('archivo: {}'.format(dir_path + os.sep + filepath))
+    if filepath.endswith('.csv'):
+        stream = open(dir_path + os.sep + filepath, 'r', encoding='utf-8')
         reader = csv.reader(stream, delimiter=",")
 
         preguntas = []
         for i, line in enumerate(reader):
-            preguntas.append(line[1].strip())
-            preguntas.append(line[2].strip())
-        conv = ["conversaciones", grouplen(preguntas, 2)]
-        with open("data/preguntas.yaml", "w", encoding="utf-8") as file:
-            yaml.dump(grouplen(conv, 2), file)
+            preguntas.append({line[1].strip(): [line[2]]})
+
+        with open('data/preguntas.yaml', 'w', encoding='utf-8') as file:
+            yaml.dump(preguntas, file)
