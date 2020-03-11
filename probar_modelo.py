4841a0c159378fe66364b28ec346a2c7adc8fb61
Felix Alejandro Guzman <felixalejandro@outlook.com>
2020-03-11 13:18:56 -0400

    Agregando prueba de modelo

--- probar_modelo.py
+++ probar_modelo.py
@@ -1,23 +1,23 @@
-import tensorflow as tf
 import yaml
-from matplotlib import pyplot
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, LSTM, Dense
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.layers import Input
 import numpy as np
 
-batch_size = 32  # Batch size for training.
-epochs = 50  # Number of epochs to train for.
+batch_size = 64  # Batch size for training.
+epochs = 100  # Number of epochs to train for.
 latent_dim = 256  # Latent dimensionality of the encoding space.
 num_samples = 100  # Number of samples to train on.
 # Path to the data txt file on disk.
 data_path = 'data/salida.yml'
 
-# Vectorize the data.
+# Vectorize the data.  We use the same approach as the training script.
+# NOTE: the data must be identical, in order for the character -> integer
+# mappings to be consistent.
+# We omit encoding target_texts since they are not needed.
 input_texts = []
 target_texts = []
 input_characters = set()
 target_characters = set()
-
 stream = open(data_path, "rb")
 docs = yaml.safe_load(stream)
 conversations = docs["conversaciones"]
@@ -54,96 +54,28 @@ target_token_index = dict(
 encoder_input_data = np.zeros(
     (len(input_texts), max_encoder_seq_length, num_encoder_tokens),
     dtype='float32')
-decoder_input_data = np.zeros(
-    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
-    dtype='float32')
-decoder_target_data = np.zeros(
-    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
-    dtype='float32')
 
-for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
+for i, input_text in enumerate(input_texts):
     for t, char in enumerate(input_text):
         encoder_input_data[i, t, input_token_index[char]] = 1.
-    encoder_input_data[i, t + 1:, input_token_index[' ']] = 1.
-    for t, char in enumerate(target_text):
-        # decoder_target_data is ahead of decoder_input_data by one timestep
-        decoder_input_data[i, t, target_token_index[char]] = 1.
-        if t > 0:
-            # decoder_target_data will be ahead by one timestep
-            # and will not include the start character.
-            decoder_target_data[i, t - 1, target_token_index[char]] = 1.
-    decoder_input_data[i, t + 1:, target_token_index[' ']] = 1.
-    decoder_target_data[i, t:, target_token_index[' ']] = 1.
-# Define an input sequence and process it.
-encoder_inputs = Input(shape=(None, num_encoder_tokens))
-encoder = LSTM(latent_dim, return_state=True)
-encoder_outputs, state_h, state_c = encoder(encoder_inputs)
-# We discard `encoder_outputs` and only keep the states.
-encoder_states = [state_h, state_c]
 
-# Set up the decoder, using `encoder_states` as initial state.
-decoder_inputs = Input(shape=(None, num_decoder_tokens))
-# We set up our decoder to return full output sequences,
-# and to return internal states as well. We don't use the
-# return states in the training model, but we will use them in inference.
-decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
-decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
-                                     initial_state=encoder_states)
-decoder_dense = Dense(num_decoder_tokens, activation='softmax')
-decoder_outputs = decoder_dense(decoder_outputs)
+# Restore the model and construct the encoder and decoder.
+model = load_model('s2s.h5')
 
-# Define the model that will turn
-# `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
-model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
-
-# Run training
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy',
-              metrics=['accuracy'])
-
-model.summary()
-
-history = model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
-                    batch_size=batch_size,
-                    epochs=epochs,
-                    validation_split=0.2)
-
-# print(history.history['loss'])
-pyplot.plot(history.history['loss'])
-pyplot.plot(history.history['val_loss'])
-pyplot.title('model train vs validation loss')
-pyplot.ylabel('loss')
-pyplot.xlabel('epoch')
-pyplot.legend(['train', 'validation'], loc='upper right')
-pyplot.show()
-
-pyplot.plot(history.history['accuracy'])
-pyplot.plot(history.history['val_accuracy'])
-pyplot.title('model train vs validation accuracy')
-pyplot.ylabel('accuracy')
-pyplot.xlabel('epoch')
-pyplot.legend(['train', 'validation'], loc='upper right')
-pyplot.show()
-
-# Save model
-model.save('s2s.h5')
-
-# Next: inference mode (sampling).
-# Here's the drill:
-# 1) encode input and retrieve initial decoder state
-# 2) run one step of decoder with this initial state
-# and a "start of sequence" token as target.
-# Output will be the next target token
-# 3) Repeat with the current target token and current states
-
-# Define sampling models
+encoder_inputs = model.input[0]  # input_1
+encoder_outputs, state_h_enc, state_c_enc = model.layers[2].output  # lstm_1
+encoder_states = [state_h_enc, state_c_enc]
 encoder_model = Model(encoder_inputs, encoder_states)
 
-decoder_state_input_h = Input(shape=(latent_dim,))
-decoder_state_input_c = Input(shape=(latent_dim,))
+decoder_inputs = model.input[1]  # input_2
+decoder_state_input_h = Input(shape=(latent_dim,), name='input_3')
+decoder_state_input_c = Input(shape=(latent_dim,), name='input_4')
 decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
-decoder_outputs, state_h, state_c = decoder_lstm(
+decoder_lstm = model.layers[3]
+decoder_outputs, state_h_dec, state_c_dec = decoder_lstm(
     decoder_inputs, initial_state=decoder_states_inputs)
-decoder_states = [state_h, state_c]
+decoder_states = [state_h_dec, state_c_dec]
+decoder_dense = model.layers[4]
 decoder_outputs = decoder_dense(decoder_outputs)
 decoder_model = Model(
     [decoder_inputs] + decoder_states_inputs,
@@ -157,6 +89,7 @@ reverse_target_char_index = dict(
     (i, char) for char, i in target_token_index.items())
 
 
+# Decodes an input sequence.  Future work should support beam search.
 def decode_sequence(input_seq):
     # Encode the input as state vectors.
     states_value = encoder_model.predict(input_seq)
